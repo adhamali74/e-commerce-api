b22dbc7d76f5f49922d540aa74ced61010b98811
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2024-03-04 23:48:10 +0100

    detailed error

--- server.js
+++ server.js
@@ -25,12 +25,19 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/categories", categoryRoute);
 //creating error and send it to error handler middleware
 app.all("*", (req, res, next) => {
-  next(new ApiError("cant find this route" + req.originalUrl, 400));
+  next(new ApiError("cant find this route" + req.originalUrl, 500));
 });
 
 //global error handling
 app.use((err, req, res, next) => {
-  res.status(400).json({ err });
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 app.listen(process.env.PORT, () => {
